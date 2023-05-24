f2f82e79c80ed5f1c644e40ecb8a23b8a7cc57ff
somani09 <vsomani2@gmail.com>
2023-05-24 22:31:27 +0530

    Community made responsive

--- components/communityContainer/communityContainer.js
+++ components/communityContainer/communityContainer.js
@@ -4,21 +4,21 @@ const CommunityContainer = () => {
   return (
     <div className={styles.communityContainer}>
         <div className={styles.jtc}>
-            <div className={styles.jtcInfo}>
-                <div className={styles.jtcInfoHeader}>Join The Community</div>
-                <div className={styles.jtcInfoText}>
+            <div  className={`${styles.info} ${styles.jtcElement}`}>
+                <div  className={styles.infoHeader}>Join The Community</div>
+                <div  className={styles.infoText}>
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                 </div>
             </div>
-            <div className={styles.jtcImage}>some random image</div>
+            <div className={`${styles.jtcImage} ${styles.jtcElement}`}>some random image</div>
         </div>
 
         <div className={styles.uta}>
             <div className={styles.utaImage}>some random image</div>
-            <div className={styles.utaInfo}>
-                <div className={styles.utaInfoHeader}>Unleash The Artist</div>
-                <div className={styles.utaInfoText}>
+            <div className={styles.info}>
+                <div className={styles.infoHeader}>Unleash The Artist</div>
+                <div className={styles.infoText}>
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                 </div>
