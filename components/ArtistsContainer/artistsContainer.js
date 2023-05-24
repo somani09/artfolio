c1e6090d74b662985e91f4397a86f10067c7a688
somani09 <vsomani2@gmail.com>
2023-05-24 13:57:35 +0530

    scss cleanup

--- components/ArtistsContainer/artistsContainer.js
+++ components/ArtistsContainer/artistsContainer.js
@@ -5,7 +5,7 @@ import styles from './artists.module.scss'
 
 const ArtistsContainer = () => {
 
-  const dummyArray = [1,2,3,4,5];
+  const dummyArray = [1,2,3,4,5,6,7,8,9,10];
 
   return (
     <div className={styles.artistContainer}>
