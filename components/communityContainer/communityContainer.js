01c70a915bcd84eb688e7a81856862c3b829d884
somani09 <vsomani2@gmail.com>
2023-05-25 13:39:26 +0530

    figuring aspect ratio

--- components/communityContainer/communityContainer.js
+++ components/communityContainer/communityContainer.js
@@ -4,18 +4,18 @@ const CommunityContainer = () => {
   return (
     <div className={styles.communityContainer}>
         <div className={styles.jtc}>
-            <div  className={`${styles.info} ${styles.jtcElement}`}>
+            <div  className={`${styles.info} ${styles.jtcText}`}>
                 <div  className={styles.infoHeader}>Join The Community</div>
                 <div  className={styles.infoText}>
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                 </div>
             </div>
-            <div className={`${styles.jtcImage} ${styles.jtcElement}`}>some random image</div>
+            <div className={`${styles.jtcImage} ${styles.image}`}>some random image</div>
         </div>
 
         <div className={styles.uta}>
-            <div className={styles.utaImage}>some random image</div>
+            <div className={`${styles.utaImage} ${styles.image}`}>some random image</div>
             <div className={styles.info}>
                 <div className={styles.infoHeader}>Unleash The Artist</div>
                 <div className={styles.infoText}>
