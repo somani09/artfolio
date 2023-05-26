30b3cdccc9dc0d2f5239d148a251e351be4c013d
somani09 <vsomani2@gmail.com>
2023-05-26 18:04:53 +0530

    working on community

--- components/communityContainer/communityContainer.js
+++ components/communityContainer/communityContainer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styles from './community.module.scss'
+import jtcImage from '../../public/assets/jtc.jpg'
+import utaImage from '../../public/assets/uta.jpg'
+import Image from 'next/image'
 const CommunityContainer = () => {
   return (
     <div className={styles.communityContainer}>
@@ -11,11 +14,21 @@ const CommunityContainer = () => {
                     Exercitation minim et ipsum nostrud occaecat elit. Dolor eu deserunt labore culpa eiusmod eu magna dolore laborum eu. Eiusmod ex ex dolor Lorem ad nisi eiusmod deserunt culpa cupidatat dolor commodo do eu.
                 </div>
             </div>
-            <div className={`${styles.jtcImage} ${styles.image}`}>some random image</div>
+            <div className={`${styles.jtcImage} ${styles.imageArea}`}>
+                <Image  
+                    className={styles.image}
+                    src={jtcImage} 
+                    />
+            </div>
         </div>
 
         <div className={styles.uta}>
-            <div className={`${styles.utaImage} ${styles.image}`}>some random image</div>
+            <div className={`${styles.utaImage} ${styles.imageArea}`}>
+                <Image  
+                    className={styles.image}
+                    src={utaImage} 
+                    />
+            </div>
             <div className={styles.info}>
                 <div className={styles.infoHeader}>Unleash The Artist</div>
                 <div className={styles.infoText}>
