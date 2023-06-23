00cf932dd758608e37025e4a24c72190a5910de7
somani09 <vsomani2@gmail.com>
2023-06-23 22:15:32 +0530

    faked data where required

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,17 @@
 import Error404 from '@/components/errors/error404'
+import Head from 'next/head'
 import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <Error404 />
+    <>
+      <Head>
+        <title>404: Page Stolen</title>
+        <meta name="description" content="404 error page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Error404 />
+    </>
   )
 }
 
