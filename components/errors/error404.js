660a2d45ece3ab1646ec1022bb85b5c62b0c5eef
somani09 <vsomani2@gmail.com>
2023-06-24 23:50:28 +0530

    minor escape

--- components/errors/error404.js
+++ components/errors/error404.js
@@ -11,7 +11,7 @@ const Error404 = () => {
                     <p className={styles.fourOfour}>
                         404
                     </p>
-                    <p className={styles.messageInfo}>Seems like someone took this art off for an unauthorized exhibition. We're on the hunt to reclaim it</p>
+                    <p className={styles.messageInfo}>Seems like someone took this art off for an unauthorized exhibition. We&#39;re on the hunt to reclaim it</p>
                 </div>
             </TapedPaper>
             
